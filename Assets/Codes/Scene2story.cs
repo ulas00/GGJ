e88466e152e8cc529771d9e0a80196e89b469fc2
Bora <borao56@gmail.com>
2023-02-05 02:17:28 +0300

    text

--- Assets/Codes/Scene2story.cs
+++ Assets/Codes/Scene2story.cs
@@ -8,12 +8,10 @@ public class Scene2story : MonoBehaviour
 {
     public GameObject gameobject;
     public TMP_Text storyText;
-
+    public AudioSource audio2;
     #region StoryTexts
     private string text1 = "Moray Bey'in lokanta zinciri çığ gibi büyümektedir. Nuray ile evlenir. Bu evlilikten nur gibi ikizleri Oray ve Koray doğar. ";
-    private string text2 = "Moray Bey iyi niyetinden Eşber'i evlatlık edinir. Ona öz oğlu gibi bakar. Eşber Bey'in ona oyun oynadığından bihaberdir.\r\nEşber Bey, Moray Bey'e \"Tepeye kadar yarışalım babacım der\". Moray Bey tepeye koşar.";
-    private string text3 = "Moray Bey aslanları püskürtmüştür. Ancak sevgili evladının ihanetini kaldıramadığı için vefat etmiştir.";
-    private string text4 = "Yorulmuşum babacım, birazdan sana yetişirim";//geri donuste event text
+    private string text2 = "Moray Bey iyi niyetinden Eşber'i evlatlık edinir. Ona öz oğlu gibi bakar. Eşber Bey'in ona oyun oynadığından bihaberdir.";
 
     #endregion
 
@@ -21,36 +19,20 @@ public class Scene2story : MonoBehaviour
     {
         Time.timeScale = 0;
         storyText.text = text1;//giristeki hikaye
+        
     }
 
     void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.O))//sahne  text 2
-        {
-            gameobject.SetActive(true);
+    {       
+            if (Input.GetKeyDown(KeyCode.Mouse0)) //cikis icin 
+            {
             storyText.text = text2;
-            Time.timeScale = 0;
-        }
-
-        if (Input.GetKeyDown(KeyCode.U))//aslanlari kestikten sonra gelen text
-        {
-            gameobject.SetActive(true);
-            storyText.text = text3;
-            Time.timeScale = 0;
-        }
-
-        if (Input.GetKeyDown(KeyCode.L))//esbere donerse
-        {
-            gameobject.SetActive(true);
-            storyText.text = text4;
-            Time.timeScale = 0;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse0)) //cikis icin 
-        {
-            Time.timeScale = 1;
-            gameobject.SetActive(false);
-        }
-
+            audio2.Play();
+            }
+           if (Input.GetKeyDown(KeyCode.E))
+            {
+                Time.timeScale = 1;
+                gameobject.SetActive(false);
+            }
     }
 }
